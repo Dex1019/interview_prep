9ce07b3c0073c515e8ef2cc6869015f2daf7e205
dex1019 <ranjan.praful@gmail.com>
2020-12-27 15:21:12 +0530

    refactor twopointers directory

--- twopointers/RemoveDuplicates.kt
+++ twopointers/RemoveDuplicates.kt
@@ -1,18 +1,16 @@
 package twopointers
 
 fun main() {
-//    val arr  = intArrayOf(2,3,3,3,6,9,9)
-//    print(removeDuplicatesFromSortedArray(arr))
+    val arr1 = intArrayOf(2, 3, 3, 3, 6, 9, 9)
+    print(removeDuplicatesFromSortedArray(arr1))
 
-
-    val arr = intArrayOf(2, 11, 2, 2, 1)
+    val arr2 = intArrayOf(2, 11, 2, 2, 1)
     val key = 2
-    print(removeDuplicatesFromUnSortedArray(arr, key))
+    print(removeDuplicatesFromUnSortedArray(arr2, key))
 }
 
 fun removeDuplicatesFromSortedArray(arr: IntArray): Int {
     var next = 1
-
     for (start in 1 until arr.size) {
         if (arr[next - 1] != arr[start]) {
             arr[next] = arr[start]
